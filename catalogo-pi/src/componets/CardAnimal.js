20d406e3d9dca5c7cbdcbf4c2932b009f001b252
DM0k <129907524+DM0k@users.noreply.github.com>
2024-02-22 22:25:35 -0300

    Construindo Cards

--- catalogo-pi/src/componets/CardAnimal.js
+++ catalogo-pi/src/componets/CardAnimal.js
@@ -4,11 +4,11 @@ import { useState } from "react";
 export default function CardAnimal(props) {
     return (
         <div className="card">
-            <div className="card-body">
-                <Image src={"/assets/blue-whale.jpg"} width={"286"} height={"388"}></Image>
+            <div className="card-body cardTemp">
+                <Image src={props.Imagem} width={"400"} height={"357"}></Image>
                 <h4 className="card-title">Card title</h4>
                 <p className="card-text">Some example text. Some example text.</p>
-                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Card link</a><br></br>
                 <a href="#" className="card-link">Another link</a>
             </div>
         </div>
--- catalogo-pi/src/pages/index.js
+++ catalogo-pi/src/pages/index.js
@@ -3,8 +3,47 @@ import CardAnimal from "@/componets/CardAnimal";
 
 export default function Home() {
   return (
-    <div className="container-fluid">
+    <div className="container-fluid fundo">
       <div className="row">
+        <div className="col-3">
+          <CardAnimal Imagem="/assets/blue-whale.jpg"></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal Imagem="/assets/mantis_shwrimp.png"></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal Imagem="/assets/anemona.png"></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal Imagem="/assets/moreia_verde.png"></CardAnimal>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+          <CardAnimal Imagem="/assets/nautilus.png"></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal></CardAnimal>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+          <CardAnimal></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal></CardAnimal>
+        </div>
+        <div className="col-3">
+          <CardAnimal></CardAnimal>
+        </div>
         <div className="col-3">
           <CardAnimal></CardAnimal>
         </div>
