d88b9a630713752f95ba3365f3527fbf70882045
DM0k <129907524+DM0k@users.noreply.github.com>
2024-02-29 19:42:51 -0300

    Detalhes de UI

--- catalogo-pi/src/componets/CardAnimal.js
+++ catalogo-pi/src/componets/CardAnimal.js
@@ -6,16 +6,16 @@ export default function CardAnimal(props) {
     return (
         <div className="card">
             <div className="card-body cardTemp">
-                <Image src={props.Imagem} width={"400"} height={"357"}></Image>
+                <img src={props.Imagem} width={"100%"} height={"100%"}></img>
                 <h6 className="sciName">{props.sciName}</h6>
-                <h4 className="card-title">{props.nome}</h4>
+                <h4 className="card-title">{props.nome}</h4> 
                 <h6>Reino: {props.reino}</h6>
                 <h6>Filo: {props.filo}</h6>
                 <h6>Classe: {props.aClass}</h6>
                 <h6>Órdem: {props.ordem}</h6>
                 <h6>Família: {props.fam}</h6>
                 <h6>Gênero: {props.gen}</h6>
-                <h6>Espécie: {props.espec}</h6>
+                <h6>Espécie: {props.espec}</h6><br></br>
                 <p className="card-text">{props.descricao}</p>
             </div>
         </div>
--- catalogo-pi/src/pages/index.js
+++ catalogo-pi/src/pages/index.js
@@ -1,35 +1,49 @@
 import CardAnimal from "@/componets/CardAnimal";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import Cabecalho from "@/componets/Cabecalho";
+import Rodape from "@/componets/Rodape";
 
 
 export default function Home() {
 
   const [listaAnimais, setListaAnimais] = useState([]);
 
-  axios.get('https://')
+
+
+  useEffect(() => {
+    axios.get('https://localhost:7017/api/Animais/')
+      .then(res => setListaAnimais(res.data));
+  }, [])
 
   return (
-    <div className="container-fluid fundo">
-      <div className="row">
-        <div className="col-3">
-          {
-          listaAnimais.map((dados, index) => <CardAnimal 
-          Imagem={dados.imagem}  
-          sciName={dados.sciName} 
-          nome={dados.nome}
-          reino={dados.reino}
-          filo={dados.filo}
-          aClass={dados.aClass}
-          ordem={dados.ordem}
-          fam={dados.fam}
-          gen={dados.gen}
-          espec={dados.espec}
-          descricao={dados.descricao} ></CardAnimal>
-          )
-          }
+
+    <><Cabecalho></Cabecalho>
+      <div className="container-fluid fundo">
+
+        <div className="row">
+          <div className="col-3">
+            {
+              listaAnimais.map((dados, index) => <CardAnimal key={index}
+                Imagem={dados.imagem}
+                sciName={dados.sciNome}
+                nome={dados.nome}
+                reino={dados.reino}
+                filo={dados.filo}
+                aClass={dados.aClass}
+                ordem={dados.ordem}
+                fam={dados.fam}
+                gen={dados.gen}
+                espec={dados.espec}
+                descricao={dados.descricao} ></CardAnimal>
+              )
+            }
+          </div>
+
         </div>
-        
+        <Rodape></Rodape>
       </div>
-    </div>
+      
+    </>
   );
 }
